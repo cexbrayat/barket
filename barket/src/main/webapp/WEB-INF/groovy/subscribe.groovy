52466ee0f17181362cd14ce530d2fa084b015b10
Agnes Crepet <agnes.crepet@gmail.com>
2011-06-18 19:01:41 +0200

    suite form inscription

---
+++ barket/src/main/webapp/WEB-INF/groovy/subscribe.groovy
@@ -0,0 +1,39 @@
+html.html()
+{
+    head()
+    {
+        title 'BARCAMP'
+    }
+    body()
+    {
+        h1 'Inscrivez-vous au plus hype des barcamps de Lyon!'
+        form(method : 'GET' , action : 'LoginAction.groovy')
+        {
+            tr()
+            {
+                td("Nom")
+                input(type : 'text' , name : 'Nom')
+            }
+            tr()
+            {
+                td("Prenom:")
+                input(type : 'text' , firstname : 'Prenom')
+            }
+            tr()
+            {
+                td("Email:")
+                input(type : 'email' ,  code : 'Email')
+            }
+            tr()
+            {
+                td("Code:")
+                input(type : 'code' ,  code : 'Code')
+            }
+
+            tr()
+            {
+                input(type : 'submit')
+            }
+        }
+    }
+}
